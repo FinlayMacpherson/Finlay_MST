b14355b293ddf89cf146739be6d631f92fbe79b1
Finlay Macpherson <Finlay.macpherson@hotmail.co.uk>
2026-04-20 00:33:06 +0100

    Graph logic added

--- graph.py
+++ graph.py
@@ -1,9 +1,21 @@
 # --- Finlay Macpherson --- Data Structures and Algorithms Assessment ---
 #The Graph will take in a collection of points
 
-import math
+import numpy as np
 
 
 class Graph:
-    def __init__(self):
-        self.points = []
+    def __init__(Self): # Initialise an empty list to store all the points
+        Self.Points = []
+
+    def AddPoints(Self, Point):
+        Self.Points.append(Point)  # Append a new point
+
+    def GetEdges(Self):
+        Edges = [] # This will hold all the edges as tuples with both points connected by distance
+        for i in range(len(Self.Points)):
+            for j in range(i + 1, len(Self.Points)):
+                P1, P2 = Self.Points[i], Self.Points[j]
+                Distance = np.linalg.norm(P1 - P2)
+                Edges.append([Distance, P1, P2])
+        return Edges
