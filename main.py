aa441333ac3523c4adcf63581e6cff27cf20b577
Finlay Macpherson <Finlay.macpherson@hotmail.co.uk>
2026-04-21 23:46:12 +0100

    Main function added

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 # --- Finlay Macpherson --- Data Structures and Algorithms Assessment ---
-#
+
 
 import hashlib
-#import something to do with csv or pdf reading
+import numpy as np
 from graph import Graph
 from mst import KruskalMST
 from Renderer import SVGRenderer
 
+def main():
+    g = Graph()
+
+    for Point in np.loadtxt("points.csv", delimiter=","):
+        g.AddPoints(Point)
+
+    Edges = g.GetEdges()
+
+    MST = KruskalMST(Edges).Compute()
+
+    SVGRenderer(MST).Render("output.svg")
+
+    print(f"MST computed with {len(MST)} edges. Saved to output.svg.")
+
+if __name__ == "__main__":
+    main()
--- mst.py
+++ mst.py
@@ -2,5 +2,33 @@
 # implements Kruskal's algorithm to compute the MST
 
 class KruskalMST:
-    def __init__(self, edges):
-        self.edges = edges
+    def __init__(Self, Edges):
+        Self.Edges = sorted(Edges, key=lambda e: e[0])
+
+    def Find(Self, Parent, Node):
+        if Parent[Node] != Node:
+            Parent[Node] = Self.Find(Parent, Parent[Node])
+        return Parent[Node]
+
+    def Union(Self, Parent, Rank, a, b):
+        ra, rb = Self.Find(Parent, a), Self.Find(Parent, b)
+        if Rank[ra] < Rank[rb]:
+            Parent[ra] = rb
+        elif Rank[ra] > Rank[rb]:
+            Parent[rb] = ra
+        else:
+            Parent[rb] = ra
+            Rank[ra] += 1
+
+    def Compute(Self):
+        Nodes = list({p for _, p1, p2 in Self.Edges for p in (p1, p2)})
+        Parent = {n: n for n in Nodes}
+        Rank = {n:0 for n in Nodes}
+        MST = []
+
+        for Weight, p1, p2 in Self.Edges:
+            if Self.Find(Parent, p1) != Self.Find(Parent, p2):
+                Self.Union(Parent, Rank, p1, p2)
+                MST.append((Weight, p1, p2))
+
+        return MST
